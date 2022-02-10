05dac5d8d6f3bc3cc510a539b1bd830f15cc39fa
rahul <rahulsrmacs@gmail.com>
2022-02-11 01:18:21 +0530

    added relative route for other component

--- routing-app/src/app/second/second.component.ts
+++ routing-app/src/app/second/second.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
+  route: ActivatedRoute | null | undefined;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  relativeRoute() {
+    this.router.navigate(['first-component'], { relativeTo: this.route });
   }
 
 }
