cf49c16c739c0f8f7923842dfb27a990d57bef4c
rahul <rahulsrmacs@gmail.com>
2022-02-11 01:03:58 +0530

    added PageNotFoundComponent, few child components and children router

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent, 
+    children: [
+    {
+      path: 'child-a', // child route path
+      component: ChildAComponent, // child route component that the router renders
+    },
+    {
+      path: 'child-b',
+      component: ChildBComponent, // another child route component that the router renders
+    },
+  ] },
+  { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
